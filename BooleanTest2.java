d060be37f4ea29b2222ed56c718a2185484fcde9
Kaili Tang <170157589+plopbloppy@users.noreply.github.com>
2025-02-20 11:40:10 -0500

    Add files via upload

---
+++ BooleanTest2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BooleanTest2 {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter input A:");
+        boolean a = keyboard.nextBoolean();
+        System.out.println("Please enter input B:");
+        boolean b  = keyboard.nextBoolean();
+        System.out.println("Please enter input C:");
+        boolean c = keyboard.nextBoolean();
+        System.out.println("Please enter input D:");
+        boolean d  = keyboard.nextBoolean();
+        keyboard.close();
+        
+        boolean output = !(b && !d) && !((!b && !c && d) || !(a && c && !d));
+        System.out.println("The output is: " + output);
+    }
+}
